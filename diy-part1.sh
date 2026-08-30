4f60911d08e6638cc6e863c198df13179573155b
lzxorz <lzxingg@gmail.com>
2026-08-30 12:33:01 +0800

    Update diy-part1.sh
    删掉：src-git tailscale_community

--- diy-part1.sh
+++ diy-part1.sh
@@ -3,8 +3,6 @@
 
 # 仅添加 Lucky 源码（HomeProxy官方源自带，切勿在此重复添加）
 git clone https://github.com/gdy666/luci-app-lucky.git package/luci-app-lucky
-# 添加 Tailscale Community 源码仓库
-echo 'src-git tailscale_community https://github.com/Tokisaki-Galaxy/luci-app-tailscale-community' >> feeds.conf.default
 # 给权限
 chmod -R 755 ./package/luci-app-lucky/*
 
